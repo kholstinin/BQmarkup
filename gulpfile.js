61aabfb98c9c133782b964e534147c0a09b627fb
Dmitry Kholstinin <kholstinin.da@gmail.com>
2017-08-06 20:58:12 +0500

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var livereload = require('gulp-livereload');
+
+gulp.task('sass', function () {
+    return gulp.src('./sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'))
+        .pipe(livereload());
+});
+
+gulp.task('watch', function () {
+    livereload.listen();
+    gulp.watch('./sass/**/*.scss', ['sass']);
+});

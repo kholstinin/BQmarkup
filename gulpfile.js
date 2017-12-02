08ad59855867a3bedd30cb7eb5daed7f512f1c65
Dmitry Kholstinin <kholstinin.da@gmail.com>
2017-12-02 22:22:01 +0500

    Добавил личный кабинет

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,21 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var livereload = require('gulp-livereload');
 
-gulp.task('sass', function () {
-    return gulp.src('./sass/*.scss')
+gulp.task('sass_acc', function () {
+    return gulp.src('./sass/accountStyles.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./css'))
         .pipe(livereload());
 });
 
+gulp.task('sass_main', function () {
+  return gulp.src('./sass/styles.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'))
+    .pipe(livereload());
+});
+
 gulp.task('watch', function () {
     livereload.listen();
-    gulp.watch('./sass/**/*.scss', ['sass']);
+    gulp.watch('./sass/**/**/*.scss', ['sass_acc', 'sass_main']);
 });
